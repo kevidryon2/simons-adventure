86e210aa182851deba7e231eff98fbd1161c9cfa
kevidryon2 <69092380+kevidryon2@users.noreply.github.com>
2021-04-05 22:57:59 +0200

    Imported assemts from code.org

---
+++ images.g.ts
@@ -0,0 +1,105 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "player_idle":
+            case "anim1":return [img`
+. . f . f . f . 
+. f f f d f . . 
+. . d d d . . . 
+. . d d d . . . 
+. . 9 6 . . . . 
+. . . 8 d . . . 
+. . . 7 . . . . 
+. . . e e . . . 
+`, img`
+. . f . f . f . 
+. f f f d f . . 
+. . d d d . . . 
+. d d d d . . . 
+. . 9 6 . . . . 
+. . . 8 . . . . 
+. . . 7 . . . . 
+. . . e e . . . 
+`];
+            case "player_tripping":
+            case "anim2":return [img`
+. . f . f . f . 
+. f f f d f . . 
+. . d d d . . . 
+. . d d d . . . 
+. d 9 6 . . . . 
+. . . 8 . . . . 
+. . 7 . . . . . 
+. e e . . . . . 
+`, img`
+. . . f . f . f 
+. . f f f d f . 
+. . . d d d . . 
+. . . d d d . . 
+. . d 9 6 . . . 
+. . . . 8 . . . 
+. . . 7 . . . . 
+. e e . . . . . 
+`];
+            case "player_skitting":
+            case "anim3":return [img`
+. . f . f . f . 
+. f f f d f . . 
+. . d d d . . . 
+. . d d d . . . 
+. d 9 6 . . . . 
+. . . 8 . . . . 
+. . 7 . . . . . 
+. e e . . . . . 
+`, img`
+. . f . f . f . 
+. f f f d f . . 
+. . d d d . . . 
+. . d d d . . . 
+. d 9 6 . . . . 
+. . . 8 . . . . 
+. . 7 . . . . . 
+. . e e . . . . 
+`, img`
+. . f . f . f . 
+. f f f d f . . 
+. . d d d . . . 
+. . d d d . . . 
+. d 9 6 . . . . 
+. . . 8 . . . . 
+. . . 7 . . . . 
+. . . e e . . . 
+`, img`
+. . f . f . f . 
+. f f f d f . . 
+. . d d d . . . 
+. . d d d . . . 
+. d 9 6 . . . . 
+. . . 8 . . . . 
+. . . . 7 . . . 
+. . . . e e . . 
+`, img`
+. . f . f . f . 
+. f f f d f . . 
+. . d d d . . . 
+. . d d d . . . 
+. d 9 6 . . . . 
+. . . 8 . e . . 
+. . . . 7 e . . 
+. . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
